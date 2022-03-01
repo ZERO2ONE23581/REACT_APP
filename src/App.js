910f24f13a475f71dd40659a0b3d99bcffd78be5
aiden <zero2one23581@gmail.com>
2022-03-01 13:55:18 +0900

    use effect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import styles from "./App.module.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
   const onClick = () => setValue((prev) => prev + 2);
-  console.log("I don't like this to be rendered after first click!");
+
+  console.log("I run all the time!");
+
+  //When you don't want something to re-render after first rendering
+  useEffect(() => {
+    console.log("Call the API");
+  }, []);
+
   return (
     <div>
       <h1 className={styles.title}>{counter}</h1>
