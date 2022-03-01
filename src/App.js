7a3fa4a482fe333a22c0fbd7ee279259d807fd34
aiden <zero2one23581@gmail.com>
2022-03-01 14:53:57 +0900

    clean up

--- src/App.js
+++ src/App.js
@@ -1,48 +1,23 @@
-import styles from "./App.module.css";
 import { useEffect, useState } from "react";
+import styles from "./App.module.css";
+
+function Hello() {
+  useEffect(() => {
+    console.log("CREATED :)");
+    return () => console.log("DESTROYED :(");
+  }, []);
+  return <h1 className={styles.btn}>HELLO WORLD</h1>;
+}
 
 function App() {
-  const [counter, setValue] = useState(0);
-  const onClick = () => setValue((prev) => prev + 2);
-
-  const [keyword, setKeyword] = useState("");
-  const onChange = (event) => {
-    setKeyword(event.target.value);
+  const [showing, setShowing] = useState(false);
+  const onClick = () => {
+    setShowing((prev) => !prev);
   };
-
-  console.log("THIS RUNS ALL THE TIME!");
-
-  useEffect(() => {
-    console.log("RUNS ONLY ONCE!");
-  }, []);
-
-  useEffect(() => {
-    console.log("RUNS WHEN 'counter' CHANGES");
-  }, [counter]);
-
-  useEffect(() => {
-    if (keyword !== "" && keyword.length > 5) {
-      console.log("RUNS WHEN 'keyword' WHERE ITS LENGTH MORE THAN 5 CHANGES");
-    }
-  }, [keyword]);
-
-  useEffect(() => {
-    console.log("I RUN WHEN 'keyword' && 'counter' CHANGES");
-  }, [keyword, counter]);
-
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        className={styles.inp}
-        type="text"
-        placeholder="Min length is 5.."
-      ></input>
-      <h1 className={styles.title}>{counter}</h1>
-      <button onClick={onClick} className={styles.btn}>
-        Click
-      </button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
