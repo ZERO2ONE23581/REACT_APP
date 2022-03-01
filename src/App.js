75f8e09e3dfc2935b826ead72f3b48e78ec10a90
aiden <zero2one23581@gmail.com>
2022-03-01 17:30:20 +0900

    To Do List

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import { useEffect, useState } from "react";
-import styles from "./App.module.css";
-
-function Hello() {
-  useEffect(() => {
-    console.log("CREATED :)");
-    return () => console.log("DESTROYED :(");
-  }, []);
-  return <h1 className={styles.btn}>HELLO WORLD</h1>;
-}
+import { useState } from "react";
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => {
-    setShowing((prev) => !prev);
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]); //todo database
+
+  const onChange = (event) => {
+    setToDo(event.target.value);
   };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return; //if there's nothing typed, return nothing
+    }
+    setToDo(""); //submit -> empty the input
+    setToDos((currentArray) => [toDo, ...currentArray]); //toDos === [] === currentArray
+    console.log(toDo, toDos);
+  };
+
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      <h1>My To-dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do..."
+        />
+        <button>Add To-Do</button>
+      </form>
     </div>
   );
 }
