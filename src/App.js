f3b633296be4313f3212512003f6a2a72264b33b
aiden <zero2one23581@gmail.com>
2022-03-01 13:47:39 +0900

    effects issue

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import Button from "./Button";
 import styles from "./App.module.css";
+import { useState } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 2);
+  console.log("I don't like this to be rendered after first click!");
   return (
     <div>
-      <h1 className={styles.title}>HELLO WORLD</h1>
-      <Button text={"HELLO WORLD"} />
+      <h1 className={styles.title}>{counter}</h1>
+      <button className={styles.btn} onClick={onClick}>
+        Click
+      </button>
     </div>
   );
 }
